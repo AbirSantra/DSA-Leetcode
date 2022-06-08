f055d2ce269cf11988375f0346570306737cf3c9
Abir Santra <santraabir8@gmail.com>
2022-06-08 22:50:56 +0530

    Added arrays package

---
+++ src/Arrays/SampleArrayProgram.java
@@ -0,0 +1,7 @@
+package Arrays;
+
+public class SampleArrayProgram {
+    public static void main(String[] args) {
+        System.out.println("This is the first program of the arrays sections");
+    }
+}
