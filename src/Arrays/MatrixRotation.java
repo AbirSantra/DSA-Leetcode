64707b975bb0584a648330834a7c10ac07b55d5f
Abir Santra <santraabir8@gmail.com>
2022-08-24 19:49:03 +0530

    Fixed Untracked Files

---
+++ src/Arrays/MatrixRotation.java
@@ -0,0 +1,13 @@
+package Arrays;
+//Question: https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/
+public class MatrixRotation {
+    public static void main(String[] args) {
+        int[][] mat = new int[][]{
+                {0,1},
+                {1,0}
+        };
+        int [][] target = new int[][]{{1,0},{0,1}};
+    }
+
+    public static void main
+}
