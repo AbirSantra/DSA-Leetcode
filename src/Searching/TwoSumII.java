a2b763ce8fbb00cf1baa911f31668a52b05a5af8
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-01-26 08:55:14 +0530

    Added new solution for Two Sum II

--- src/Searching/TwoSumII.java
+++ src/Searching/TwoSumII.java
@@ -36,3 +36,32 @@ class Solution {
         return -1;
     }
 }
+
+// BETTER SOLUTION
+/**
+Two Pointer Intuition: We start by considering the elements at the extreme ends (that is, the smallest element and the largest element). Now if the sum of these two elements is equal to the target, we have found our answer. But, if the sum of these two elements is more than the target, we need to reduce our sum. So we have to consider the next largest element, i.e, we move the end-pointer one step inwards. However, if the sum is less than the target, then we need to try and increase the sum. So we have to consider the next smallest element, i.e we move the start-pointer one step inwards. Eventually, we will find our answer.
+*/
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[]{-1,-1};
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while(start < end){
+            int sum = numbers[start] + numbers[end];
+
+            if(sum == target){
+                ans[0] = start + 1;
+                ans[1] = end + 1;
+                break;
+            } else if(sum > target){
+                end--;
+            } else {
+                start++;
+            }
+        }
+
+        return ans;
+    }
+}
