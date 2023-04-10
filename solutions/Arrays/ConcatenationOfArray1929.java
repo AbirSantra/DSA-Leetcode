8c90d5f1bc059f0bc2588aa718bb5c19b5329521
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-04-10 20:00:51 +0530

    Update ConcatenationOfArray1929.java

--- solutions/Arrays/ConcatenationOfArray1929.java
+++ solutions/Arrays/ConcatenationOfArray1929.java
@@ -1,20 +1,12 @@
-package Arrays;
-//https://leetcode.com/problems/concatenation-of-array/
-
-import java.util.Arrays;
-
-public class ConcatenationOfArray1929 {
-    public static void main(String[] args) {
-        int[] nums = new int[]{1,3,2,1};
-        System.out.println(Arrays.toString(getConcatenation(nums)));
-    }
-
-    public static int[] getConcatenation(int[] nums){
-        int[] ans = new int[nums.length*2];
-        for (int i=0;i<nums.length;i++){
-            ans[i]=nums[i];
-            ans[i+nums.length]=nums[i];
+// Question Link: https://leetcode.com/problems/concatenation-of-array/
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans =  new int[nums.length*2];
+        for(int i=0;i<nums.length;i++){
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
         }
+
         return ans;
     }
 }
