3f2ea10446b968a9888c48db238d9e64cf87d4c1
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-04-10 19:55:45 +0530

    Update BuildArrayFromPermutation1920.java

--- solutions/Arrays/BuildArrayFromPermutation1920.java
+++ solutions/Arrays/BuildArrayFromPermutation1920.java
@@ -1,19 +1,34 @@
-package Arrays;
-//Question: https://leetcode.com/problems/build-array-from-permutation/
-
-import java.util.Arrays;
-
-public class BuildArrayFromPermutation1920 {
-    public static void main(String[] args) {
-        int[] nums = new int[]{5,0,1,2,3,4};
-        System.out.println(Arrays.toString(buildArray(nums)));
-    }
-
-    public static int[] buildArray(int[] nums){
+// Question Link: https://leetcode.com/problems/build-array-from-permutation/
+/* O(n) Approach */
+/* class Solution {
+    public int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
         for(int i=0;i<nums.length;i++){
-            ans[i] = nums[nums[i]];
+            nums[i] = nums[nums[i]];
         }
-        return ans;
+
+        return nums;
+    }
+} */
+/* 
+ Optimized O(1) Approach
+ The idea is to store both the original number as well as the target number in the same place.
+ This can be done by replace all the numbers by using the formula a+bn where a is the original number nums[i],
+ b is the target number nums[nums[i]] and n is the length of the array.
+ Then the original number can be retrieved by nums[i]%n
+ And the target number can be retrieved by nums[i]/n 
+*/
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            nums[i] = nums[i] + n * (nums[nums[i]]%n);
+        }
+
+        for(int i=0;i<n;i++){
+            nums[i] = nums[i] / n;
+        }
+
+        return nums;
     }
 }
