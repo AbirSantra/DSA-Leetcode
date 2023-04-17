90d1b17760bac6690054ecd380ee89ecdfd97431
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-04-17 10:21:43 +0530

    Update RichestCustomerWealth1672.java

--- solutions/Arrays/RichestCustomerWealth1672.java
+++ solutions/Arrays/RichestCustomerWealth1672.java
@@ -1,22 +1,26 @@
-package Arrays;
-//Question: https://leetcode.com/problems/richest-customer-wealth/
+// Question: https://leetcode.com/problems/richest-customer-wealth/
+/* 
+Approach: We traverse throught the array and get the sum of array in the array. 
+Then we compare it with the max. If sum is greater than max, we replace max with sum.
+ */
 
-public class RichestCustomerWealth1672 {
-    public static void main(String[] args) {
-        int[][] account = new int[][]{{1,5},{7,3},{3,5}};
-        System.out.println(maximumWealth(account));
-    }
-    public static int maximumWealth(int[][] accounts){
-        int ans =0;
-        for(int i=0;i< accounts.length;i++){
-            int sum =0;
-            for(int j=0;j<accounts[i].length;j++){
-                sum = sum + accounts[i][j];
-            }
-            if(sum>ans){
-                ans = sum;
-            }
+class Solution {
+    public static int arraySum(int[] arr){
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
         }
-        return ans;
+
+        return sum;
+    }
+
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for(int i=0;i<accounts.length;i++){
+            int sum = arraySum(accounts[i]);
+            max = Math.max(sum,max);
+        }
+
+        return max;
     }
 }
