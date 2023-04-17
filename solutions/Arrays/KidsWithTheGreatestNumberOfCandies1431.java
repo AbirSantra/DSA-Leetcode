ef1592be973f28f498d160d3bce82ba048f37267
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-04-17 11:16:56 +0530

    Update KidsWithTheGreatestNumberOfCandies1431.java

--- solutions/Arrays/KidsWithTheGreatestNumberOfCandies1431.java
+++ solutions/Arrays/KidsWithTheGreatestNumberOfCandies1431.java
@@ -1,22 +1,25 @@
-package Arrays;
+// Question Link: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+/* 
+Approach: 
+We find the max element in the array.
+Then we traverse through the array and for each element,
+we add the extraCandies and see if the sum is more then or equal to max.
+If true, then we add true to the ans arraylist,
+Else, we add false to the ans arraylist
+ */
 
-import java.util.ArrayList;
-import java.util.List;
-
-//Question: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
-public class KidsWithTheGreatestNumberOfCandies1431 {
-    public static void main(String[] args) {
-        int[] candies = new int[]{4,2,1,1,2};
-        System.out.println(kidsWithCandies(candies,1));
-    }
-    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies){
-        List<Boolean> ans = new ArrayList<Boolean>();
-        int max =0;
-        for (int i=0;i<candies.length;i++){
-            if(max<candies[i]){
-                max = candies[i];
-            }
+class Solution {
+    public static int findMax(int[] candies){
+        int max = 0;
+        for(int i=0;i<candies.length;i++){
+            max = Math.max(max,candies[i]);
         }
+
+        return max;
+    }
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<Boolean>();
+        int max = findMax(candies);
         for(int i=0;i<candies.length;i++){
             if(candies[i]+extraCandies >= max){
                 ans.add(true);
@@ -24,6 +27,7 @@ public class KidsWithTheGreatestNumberOfCandies1431 {
                 ans.add(false);
             }
         }
+
         return ans;
     }
 }
