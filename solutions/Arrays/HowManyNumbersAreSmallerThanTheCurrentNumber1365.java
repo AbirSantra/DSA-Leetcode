4b2cdde6dc4a55fd7865695147791c20c32948e4
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-05-04 10:32:07 +0530

    Update HowManyNumbersAreSmallerThanTheCurrentNumber1365.java

--- solutions/Arrays/HowManyNumbersAreSmallerThanTheCurrentNumber1365.java
+++ solutions/Arrays/HowManyNumbersAreSmallerThanTheCurrentNumber1365.java
@@ -1,52 +1,37 @@
-package Arrays;
-//Question: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
-
-import java.util.Arrays;
-
-public class HowManyNumbersAreSmallerThanTheCurrentNumber1365 {
-    public static void main(String[] args) {
-        int[] nums = new int[]{6,5,4,8};
-        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
-    }
-    //BRUTE FORCE APPROACH
-//    public static int[] smallerNumbersThanCurrent(int[] nums){
-//        int[] ans = new int[nums.length];
-//        for (int i=0;i< nums.length;i++){
-//            int count = 0;
-//            for (int j=0;j<nums.length;j++){
-//                if(i!=j && nums[i]>nums[j]) count++;
-//            }
-//            ans[i]=count;
-//        }
-//        return ans;
-//    }
-
-
-    //OPTIMIZED APPROACH
-    //First we store the frequency of each character in an array.
-    //Then we store the running sum of the previous array in another. This gives us the no of numbers smaller than the current number at a point
-    //Then we check for each character in the nums array in the runningsum array and return the no. of numbers smaller from the runningsum array.
-
-    public static int[] smallerNumbersThanCurrent(int[] nums){
+//Question Link: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+/* 
+Approach: 
+First we find out the frequency of occurence of each character.
+Then we calculate a running sum of this frequency array.
+Now the rSum at each position is equal to the total number of elements that have occurred before it
+as well as the element itself.
+Since we need only elements smaller than the current element,
+we return the rSum of the element before it.
+ */
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] freq = new int[101];
+        int[] rSum = new int[101];
         int[] ans = new int[nums.length];
-        int[] count = new int[101];
+
         for(int i=0;i<nums.length;i++){
-            count[nums[i]]++;
+            freq[nums[i]]++;
         }
-        int[] rsum = new int[101];
+
         int sum = 0;
-        for(int i=0;i<count.length;i++){
-            sum+=count[i];
-            rsum[i]=sum;
+        for(int i=0;i<freq.length;i++){
+            sum = sum + freq[i];
+            rSum[i] = sum;
         }
-        for (int i=0;i<nums.length;i++){
+
+        for(int i=0;i<nums.length;i++){
             if(nums[i]==0){
                 ans[i] = 0;
             } else {
-                ans[i] = rsum[nums[i]-1];
+                ans[i] = rSum[nums[i]-1];
             }
         }
+
         return ans;
     }
-
 }
