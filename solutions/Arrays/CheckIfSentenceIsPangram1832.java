b52328b53ad29ae1aa413fc4988f6f8d62b2da8a
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-05-05 08:20:43 +0530

    Update CheckIfSentenceIsPangram1832.java

--- solutions/Arrays/CheckIfSentenceIsPangram1832.java
+++ solutions/Arrays/CheckIfSentenceIsPangram1832.java
@@ -1,44 +1,28 @@
-package Arrays;
-//Question: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
-public class CheckIfSentenceIsPangram1832 {
-    public static void main(String[] args) {
-        String sentence = "thequickbrownfoxjumpsoverthelazydog";
-        System.out.println(checkIfPangram(sentence));
-    }
-//    O(n2) solution / Brute approach
-//    public static boolean checkIfPangram(String sentence){
-//        for(char i='a';i<='z';i++){
-//            int count =0;
-//            for(int j=0;j<sentence.length();j++){
-//                if(sentence.charAt(j)==i){
-//                    count++;
-//                }
-//            }
-//            if(count==0) return false;
-//        }
-//        return true;
-//    }
-
-//    O(n) approach using count array
-//    public static boolean checkIfPangram(String sentence){
-//        int[] alpha = new int[26]; //to store the frequency of each alphabet
-//        for(int i=0;i<sentence.length();i++){
-//            int index = (int)sentence.charAt(i)-97;
-//            alpha[index]++;
-//        }
-//        for (int i=0;i<alpha.length;i++){
-//            if(alpha[i]==0) return false;
-//        }
-//        return true;
-//    }
-
-//    Using contains() method
-    public static boolean checkIfPangram(String sentence){
-        if(sentence.length()<26) return false;
-
-        for (int i=0;i<26;i++){
-            if(!sentence.contains(Character.toString(i+97))) return false;
+//Question Link: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+/*
+Approach:
+1. If a sentence is pangram, its length must be 26 or more. Check that first
+2. Create a frequency array of size 26 which stores the occurences of all the alphabets
+3. Check if any position is empty in the freq array.
+4. If yes, return false.
+*/
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length()<26){
+            return false;
         }
+
+        int[] freq = new int[26];
+        for(int i=0;i<sentence.length();i++){
+            freq[(int)sentence.charAt(i)-97]++;
+        }
+
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]==0){
+                return false;
+            }
+        }
+
         return true;
     }
 }
