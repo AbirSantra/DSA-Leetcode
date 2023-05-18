8576795a84c149821117f200d85d6e4737d4aa49
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-05-18 08:49:22 +0530

    Update MaximumPopulationYear.java

--- solutions/Arrays/MaximumPopulationYear.java
+++ solutions/Arrays/MaximumPopulationYear.java
@@ -1,21 +1,72 @@
-package Arrays;
+// Question Link: https://leetcode.com/problems/maximum-population-year/
+/* 
+O[n^2]
+Approach:
+We consider a frequency array of the years from 1950 to 2050 which are the constraints.
+Now for each entry of the logs, we run a loop from the birth till before death
+and increment the population for those years.
+At the end, we check the freq array and return the maximum year.
+*/
 
-public class MaximumPopulationYear {
+// class Solution {
+//     public int maximumPopulation(int[][] logs) {
+//         int[] pop = new int[100];
+//         int ans = 0;
+//         for(int i=0;i<logs.length;i++){
+//             int birth = logs[i][0];
+//             int death = logs[i][1];
+//             for(int j=birth;j<death;j++){
+//                 pop[j-1950]++;
+//             }
+//         }
+
+//         int max = 0;
+//         for(int i=0;i<pop.length;i++){
+//             if(pop[i]>max){
+//                 max = pop[i];
+//                 ans = i + 1950;
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+/* 
+O[n]
+Approach Two: 
+In this approach, we make use of a prefix sum. That is, the sum of the current element
+and the previous element.
+So for each entry, we increment the birth year and decrement the death year.
+Then while checking the frequency array, we update the values with the prefix sum.
+So if birth is 1950 and death is 1960,
+Freq = [1,0,0,0,0,0,0,0,-1]
+After prefix sum updation, we get the desired freq array:
+[1,1,1,1,1,1,1,1,0]
+And then we return the max from there.
+*/
+class Solution {
     public int maximumPopulation(int[][] logs) {
-        int ans = 0;
         int[] pop = new int[101];
-        int maxPop = 0;
+        int max = 0;
+        int ans = 1950;
 
-        for(int[] log : logs){
-            for(int i=log[0];i<log[1];i++){
-                pop[i-1950]++;
-            }
+        // Increment birth year & decrement death year
+        for(int i=0;i<logs.length;i++){
+            pop[logs[i][0] - 1950]++;
+            pop[logs[i][1] - 1950]--;
         }
 
+        // Calculate the prefix sum
+        for(int i=1;i<pop.length;i++){
+            pop[i] += pop[i-1];
+        }
+
+        // Find max
         for(int i=0;i<pop.length;i++){
-            if(pop[i]>maxPop){
-                maxPop = pop[i];
-                ans = i+1950;
+            if(pop[i]>max){
+                max = pop[i];
+                ans = i + 1950;
             }
         }
 
