d0d318f93ed9fea6c9660e4bbd6d02a76ef12251
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-05-19 08:51:35 +0530

    Update Determine Whether Matrix Can Be Obtained By Rotation.java

--- solutions/Arrays/Determine Whether Matrix Can Be Obtained By Rotation.java
+++ solutions/Arrays/Determine Whether Matrix Can Be Obtained By Rotation.java
@@ -1,30 +1,44 @@
+// Question Link: https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/description/
+/* 
+Approach:
+Since there are only 4 possible 90-degree increments, (0, 90, 180, 360)
+We simply need to run the algo 4 times and return if any case is true.
+Each time, we will rotate the array and then check if it is equal to the target.
+*/
+
 class Solution {
-    public static boolean findRotation(int[][] mat, int[][] target){
+    public boolean findRotation(int[][] mat, int[][] target) {
         boolean ans = false;
-        for (int i=0;i<4;i++){
-            mat = rotateMat(mat);
-            if (isEqual(mat,target)){
-                ans = true;
-                break;
+        int n = mat.length;
+        for(int i=0;i<4;i++){
+            mat = rotateMat(mat,n);
+            if(isEqualMat(mat,target,n)){
+                return true;
             }
         }
         return ans;
     }
-    public static boolean isEqual(int[][] mat,int[][] target){
-        for (int i=0;i<mat.length;i++){
-            for (int j=0;j<mat.length;j++){
-                if (mat[i][j]!=target[i][j]) return false;
+
+    public static int[][] rotateMat(int[][] mat, int n){
+        int[][] temp = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                temp[i][j] = mat[n-j-1][i];
             }
         }
+
+        return temp;
+    }
+
+    public static boolean isEqualMat(int[][] mat, int[][] target, int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]!=target[i][j]){
+                    return false;
+                }
+            }
+        }
+
         return true;
-    }
-    public static int[][] rotateMat(int[][] mat){
-        int[][] ans = new int[mat.length][mat.length];
-        for (int i=0;i<mat.length;i++){
-            for (int j=0;j<mat.length;j++){
-                ans[i][j] = mat[mat.length-j-1][i];
-            }
-        }
-        return ans;
     }
 }
