33960cb640e07a35d64ee6d1868cb9f306834696
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-05-20 09:06:23 +0530

    Update ReshapeTheMatrix.java

--- solutions/Arrays/ReshapeTheMatrix.java
+++ solutions/Arrays/ReshapeTheMatrix.java
@@ -1,21 +1,37 @@
+// Question Link: https://leetcode.com/problems/reshape-the-matrix/description/
+/*
+Approach:
+We will traverse the original matrix and put element in the newly formed matrix.
+But we will use a row and col pointer for addressing the new matrix.
+We will add elements by incrementing the col pointer for a row.
+Once a row is finished, we will increment the pointer to the next row. 
+*/
+
 class Solution {
-    public int[][] matrixReshape(int[][] matrix, int r, int c) {
-        if(matrix.length*matrix[0].length != r*c){
-            return matrix;
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        if(m*n != r*c){
+            return mat;
         } else {
+            int rp = 0;
+            int cp = 0;
             int[][] ans = new int[r][c];
-            int rp = 0, cp = 0; // row and column pointers for the ans array
-            for(int i=0;i<matrix.length;i++){
-                for(int j=0;j<matrix[i].length;j++){
-                    ans[rp][cp] = matrix[i][j];
-                    if(cp == c-1){ // if the col pointer has reached the last column, reset to 0 and increase row pointer
-                        rp++;
-                        cp = 0;
-                    } else { // if the col pointer has no reached the last column, increase the column pointer
+
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    ans[rp][cp] = mat[i][j];
+
+                    if(cp < c-1){
                         cp++;
+                    } else {
+                        cp = 0;
+                        rp++;
                     }
                 }
             }
+
             return ans;
         }
     }
