a4f19678750e6dc64191d02a8ef758d49a522f7c
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-05-22 09:16:18 +0530

    Update PlusOne.java

--- solutions/Arrays/PlusOne.java
+++ solutions/Arrays/PlusOne.java
@@ -1,28 +1,35 @@
+// Question Link: https://leetcode.com/problems/plus-one/
+
+/* 
+Approach: 
+We start from right to left, emulating a mathematical edition.
+We will think of 1 as a carry.
+For each digit, we add 1.
+If the sum is 9 or less, we simply replace that digit with the sum,
+and return the array since all other digits will remain the same.
+Else, if the sum become 10, we simply replace the digit with 0,
+and move to the next iteration.
+If, after traversing the entire array, the carry still remains, we simply
+create a new array of N+1 size and the set the first element as 1.
+So, if the number is 999, then after traversing, the array will become 000
+and would be yet to be returned. So we create an array of size 4, which is 0000,
+and then set the first element to be 1. So we get 1000 which is the answer.
+*/
+
 class Solution {
     public int[] plusOne(int[] digits) {
-        // since we need to add only 1 to the number, we take 1 as the initial carry and add the carry to each digit one by one. If it results to 10 we add 0 to the new number and make carry 1. If sum is less than 10 then we simply add with carry and add it to the new number and make carry 0. At the end, we check if there is still a carry remaining and add 1 to the new number.
-        int carry  = 1;
-        List<Integer> newDigits = new ArrayList<>();
         for(int i=digits.length-1;i>=0;i--){
-            int sum = digits[i] + carry;
-            if(sum == 10){
-                newDigits.add(0,0);
-                carry = 1;
+            int sum = digits[i] + 1;
+            if(sum < 10){
+                digits[i] = sum;
+                return digits;
             } else {
-                newDigits.add(0,sum);
-                carry = 0;
+                digits[i] = 0;
             }
         }
-        
-        if(carry == 1){
-            newDigits.add(0,1);
-        }
-        
-        int[] ans = new int[newDigits.size()];
-        for(int i=0;i<ans.length;i++){
-            ans[i] = newDigits.get(i);
-        }
-        
-        return ans;
+
+        int[] newDigits = new int[digits.length+1];
+        newDigits[0] = 1;
+        return newDigits;
     }
 }
