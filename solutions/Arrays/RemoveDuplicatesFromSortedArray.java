172127aa85b36a0601be92921b0ed8b8458904d9
Abir Santra <78201921+AbirSantra@users.noreply.github.com>
2023-06-04 08:43:28 +0530

    Update RemoveDuplicatesFromSortedArray.java

--- solutions/Arrays/RemoveDuplicatesFromSortedArray.java
+++ solutions/Arrays/RemoveDuplicatesFromSortedArray.java
@@ -1,16 +1,26 @@
+// Question Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+/* 
+Approach:
+We are simply going to traverse the array and maintain a pointer starting from 0.
+Each time the element at the current index is not equal to the next element,
+we replace the element at the pointer with the current element and increment the pointer by 1.
+After the loop ends, we replace the element at the pointer with the last element.
+This is done because the loop will run till before the last element,
+and the last element might be a unique element.
+At the end, we return the pointer.
+*/
+
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length==0) return 0;
-        else{
-            int j=0;
-            for(int i=0;i<nums.length-1;i++){
-                if(nums[i]!=nums[i+1]){
-                    nums[j]=nums[i];
-                    j++;
-                }
+        int ptr = 0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]!=nums[i+1]){
+                nums[ptr] = nums[i];
+                ptr++;
             }
-        nums[j++]=nums[nums.length-1];
-        return j;
         }
+
+        nums[ptr++] = nums[nums.length-1];
+        return ptr;
     }
 }
